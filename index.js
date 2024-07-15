a0d0a5ec90488bf63d0fa25eed9824d9440ae893
Gaurav <royalgk2@gmail.com>
2024-07-15 15:06:46 +0530

    defined API endpoints to manage process

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const processRoutes = require('./routes/process');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,6 +17,9 @@ app.use(express.json());
 mongoose.connect(`mongodb+srv://royalgk2:${process.env.MONGO_PASS}@operatingsystem.pdn0vc2.mongodb.net/?retryWrites=true&w=majority&appName=OperatingSystem`)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
+    
+//Integrate routes into the server.
+app.use('/api/processes', processRoutes);
 
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ routes/process.js
@@ -0,0 +1,29 @@
+// backend/routes/process.js
+const express = require('express');
+const router = express.Router();
+const Process = require('../models/Process');
+
+// Create a new process
+router.post('/', async (req, res) => {
+    try {
+        const process = new Process(req.body);
+        await process.save();
+        res.status(201).send(process);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Get all processes
+router.get('/', async (req, res) => {
+    try {
+        const processes = await Process.find();
+        res.status(200).send(processes);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Update and delete routes can also be added
+
+module.exports = router;
